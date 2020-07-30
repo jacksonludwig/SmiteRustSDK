1d25fb466b0721d9cb29ba2d40d73202e700e7f2
Jackson <jacksonludwig0@gmail.com>
2020-07-30 14:51:59 -0400

    track time

--- src/queries.rs
+++ src/queries.rs
@@ -3,23 +3,19 @@ use super::json;
 /// Create an object to make queries with its given id and timestamp.
 pub struct QueryBuilder {
     session_id: String,
-    timestamp: String,
 }
 
 impl QueryBuilder {
     /// Use the session id and timestamp to make a new builder.
-    pub fn new(session_id: String, timestamp: String) -> QueryBuilder {
-        QueryBuilder {
-            session_id,
-            timestamp,
-        }
+    pub fn new(session_id: String) -> QueryBuilder {
+        QueryBuilder { session_id }
     }
 
     /// Make a "getplayer" query to the API.
     pub fn get_player(&self, name: &str) -> Result<String, reqwest::Error> {
         let link = format!(
             "{}/{}",
-            json::create_link("getplayer", &self.session_id, &self.timestamp),
+            json::create_link("getplayer", &self.session_id, &json::get_formatted_time()),
             name
         );
         json::fetch_json(&link)
